8dead748026f6b0ae9ad9773be11a5f97aa9f68d
aka-rob <rmizell@gmail.com>
2017-12-02 15:34:32 -0800

    Checkpoint 5 - Pundit added for authorization.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :wiki, dependent: :destroy
+  before_save { self.role ||= :standard }
+
+  enum role: [:standard, :premium, :admin]
 end
