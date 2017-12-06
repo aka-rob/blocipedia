c9ae5a04c27167d78cfbe58d770093bb6bf0459b
aka-rob <rmizell@gmail.com>
2017-12-05 19:47:37 -0800

    Checkpoint 7 - Stripe integration added. Downgrade added.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   resources :wikis
-  devise_for :users
+  resources :charges, only: [:new, :create]
+  devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_scope :user do
+    post 'users/registrations/downgrade' => 'users/registrations#downgrade'
+  end
   root 'welcome#index'
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
