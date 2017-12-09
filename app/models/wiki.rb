a8e5b8c4794d08be0866824862b32691d688a1c2
aka-rob <rmizell@gmail.com>
2017-12-09 10:38:24 -0800

    Checkpoint 10 - Collaborator functionality added.

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,6 +1,8 @@
 class Wiki < ApplicationRecord
   belongs_to :user
-
+  has_many :collaborators
+  has_many :users, through: :collaborators
+  
   validates :title, presence: true
   validates :body, presence: true
   validates :user, presence: true
